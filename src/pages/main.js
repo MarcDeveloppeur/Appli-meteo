116582ff29d4170a5d2d3fc8a7a521e11e363bf0
Marc <marcramadison006@gmail.com>
2021-02-01 18:39:16 +0300

    ajouter le bouton recherche

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,18 +8,21 @@ function Main() {
   const [resultat,setResultat]=useState('');
   const handleChange=(e)=>setCity(e.target.value);
   const action=(e)=>{
-    if(e.key=='Enter'){
+    e.preventDefault();
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=fr&appid=8f65f8e20e985e99a07ac6c22202d975')
     .then(data=>data.json())
     .then((res)=>setResultat(res));
-  }
+
   }
 
   return (
 
     <div className={typeof resultat.weather != 'undefined' && resultat.main.temp>12?"hotbackground":"coldbackground"}>
         <div className="search-box">
-            <input type="text" className="search" placeholder="Rechercher une ville" value={city} onChange={handleChange} onKeyPress={action}/>
+           <form onSubmit={action}>
+                <input type="text" className="search" placeholder="Rechercher un pays ou une ville" value={city} onChange={handleChange} />
+                <button className="searchButton" type="submit">Rechercher</button>
+            </form>
         </div>
         <div className="result">
 
